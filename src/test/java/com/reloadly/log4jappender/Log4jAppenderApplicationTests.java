f2956f13244eae8b8b11ee5fcfa79eeb3c5ad126
Justice nwoke <jnwoke@reloadly.com>
2022-05-06 16:52:59 +0100

    initial commit

---
+++ src/test/java/com/reloadly/log4jappender/Log4jAppenderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reloadly.log4jappender;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Log4jAppenderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
